1f81a422379db7403f73309c8c589192a273e1f5
Barrau Quentin <21703046t>
2019-03-13 15:45:29 +0100

    ajout taches

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 let list = new TaskList();
 let tasks = new CompoTaskList({
     element: document.getElementById('tasks'),
-    taskList: list
+    taskList: list,
 });
+
+tache1 = new Task("Première_tache","14/03");
+tache2 = new Task("Deuxième_tache","17/03");
+tache3 = new Task("Troisième_tache","25/03");
+
+
+
+list.add(tache1);
+console.log("ENTRE");
+
+
+
+
+
+console.log(tache1);
+console.log("ENTRE");
+console.log(list);
+console.log("ENTRE");
+console.log(taskList);
+console.log("ENTRE");
+console.log(tasks);
